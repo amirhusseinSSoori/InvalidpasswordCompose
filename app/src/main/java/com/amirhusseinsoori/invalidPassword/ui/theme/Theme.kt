cc5590cdf312406bd4e5678d7c2ab43fbd632249
amiehusseinSSoori <amirhusseinsoori75@gmail.com>
2021-10-19 23:23:17 -0700

    change name app

--- app/src/main/java/com/amirhusseinsoori/invalidPassword/ui/theme/Theme.kt
+++ app/src/main/java/com/amirhusseinsoori/invalidPassword/ui/theme/Theme.kt
@@ -6,11 +6,7 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
@@ -27,17 +23,3 @@ private val LightColorPalette = lightColors(
     */
 )
 
-@Composable
-fun MySettingAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
-) {
-    val colors = LightColorPalette
-
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
-}
